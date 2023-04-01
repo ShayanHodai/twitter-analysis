453062bdc673d720de0f27fb9e3d2edf4ec1cdb9
ShayanHodai <shayan.hodai@outlook.com>
2023-04-01 00:41:18 -0400

    environemt & requirements

---
+++ env.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# latest packages as of 2023-04-01
+
+python3 -m venv $(pwd)/venv/twitter
+source $(pwd)/venv/twitter/bin/activate
+pip install --upgrade pip
+pip install snscrape
+pip install tensorflow-cpu
+pip install pandas
+pip install Flask
+pip install pymongo
+pip install emoji==0.6.0
+pip install psutil
+pip install transformers
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+echo 
+echo 'Done! as of 2023-04-01 lastest required packages are installed succesfully.'
