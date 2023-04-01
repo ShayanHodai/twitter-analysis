c8fb72c19752c721e4983aa29ca1409db82b6289
ShayanHodai <shayan.hodai@outlook.com>
2023-04-01 01:57:12 -0500

    environment & packages

--- env.sh
+++ env.sh
@@ -13,4 +13,5 @@ pip install psutil
 pip install transformers
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 echo 
-echo 'Done! as of 2023-04-01 lastest required packages are installed succesfully.'
+echo
+echo 'Done! as of 2023-04-01 lastest version of required packages are installed succesfully.'
